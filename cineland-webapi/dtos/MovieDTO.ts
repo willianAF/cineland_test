ae0ad0fc6867ff49ff3a6032b886964e723463dc
Vito <viitoo0918@gmail.com>
2025-05-29 20:16:35 -0300

    Add TypeScript support and development scripts. Add new routes to fetch Movie and TV Show data.

---
+++ cineland-webapi/dtos/MovieDTO.ts
@@ -0,0 +1,8 @@
+export interface MovieDTO {
+    id: number;
+    title: string;
+    overview: string;
+    poster_path: string;
+    release_date: string;
+    backdrop_path: string;
+}
---
+++ cineland-webapi/dtos/TVShowDTO.ts
@@ -0,0 +1,8 @@
+export interface TVShowDTO {
+    id: number;
+    title: string;
+    overview: string;
+    posterPath: string;
+    backdropPath: string;
+    releaseDate: string;
+}
